c2ea303efe549beaabcc46ec2408be45412b9276
rafaeldgdf <rafaelvd2@hotmail.com>
2024-08-29 17:33:34 -0300

    Atualizando

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CadastroUsuario from './components/CadastroUsuario';
+
+function App() {
+  return (
+    <div className="App">
+      <CadastroUsuario />
+    </div>
+  );
+}
+
+export default App;
